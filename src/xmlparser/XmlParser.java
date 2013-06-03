18f545c99f474ca7480443a70b8c28cb0e46759c
Alina_Shumel <Alina_Shumel@EPBYMINW2702.minsk.epam.com>
2013-06-03 10:09:20 +0300

    Init commit

---
+++ src/xmlparser/XmlParser.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xmlparser;
+
+/**
+ *
+ * @author Alina_Shumel
+ */
+public class XmlParser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
